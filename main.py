f74d83d62077a19ad46d1ea1683c0df068c8aa7a
DhrumilPrajapati03 <prajapatidhrumil2022@gmail.com>
2025-11-04 14:43:57 +0530

    Add order and category endpoints to main application router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from typing import List, Optional
 from fastapi import FastAPI, File, Form, UploadFile
 from src.core.database import engine, Base
+from src.api.endpoints import users, auth, sellers, products, profile, orders, user_orders, seller_orders
 from src.api.endpoints import users, auth, sellers, products, profile, cart
 from src.core.seed import seed_admin
 from contextlib import asynccontextmanager  
@@ -60,4 +61,8 @@ app.include_router(auth.router)
 app.include_router(sellers.router)
 app.include_router(profile.router)
 app.include_router(products.router)
+app.include_router(orders.router)
+app.include_router(user_orders.router)
+app.include_router(seller_orders.router)
+app.include_router(categories.router)
 app.include_router(cart.router)
