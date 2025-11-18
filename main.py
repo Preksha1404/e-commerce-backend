c3156622a3e59beb887b46d703905ddc129fd30d
DhrumilPrajapati03 <prajapatidhrumil2022@gmail.com>
2025-11-18 15:38:41 +0530

    Implement admin analytics dashboard endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.core.database import engine, Base
-from src.api.endpoints import users, auth, sellers, products, profile, cart, addresses, payments, orders, user_orders, seller_orders, coupons, review, invoice, seller_analytics
+from src.api.endpoints import users, auth, sellers, products, profile, cart, addresses, payments, orders, user_orders, seller_orders, coupons, review, invoice, seller_analytics, analytics
 from src.core.seed import seed_admin, seed_default_category
 from contextlib import asynccontextmanager  
 from fastapi.middleware.cors import CORSMiddleware
@@ -69,3 +69,4 @@ app.include_router(coupons.router)
 app.include_router(review.router)
 app.include_router(seller_analytics.router)
 app.include_router(invoice.router)
+app.include_router(analytics.router)
